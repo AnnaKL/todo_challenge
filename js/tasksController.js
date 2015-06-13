223af39e5378c150c0a102b4dd27a34375d4906c
Anna <anna.klimas@gmail.com>
2015-06-13 14:32:16 +0100

    Tasks can be stored. Tested

--- js/tasksController.js
+++ js/tasksController.js
@@ -1,3 +1,11 @@
 tasksList.controller('TasksController', [function() {
 
+  var self = this;
+  self.allTasks = [];
+
+
+  self.printTasks = function(){
+    var newTask = {'task': self.submittedTask, 'completed': false};
+    self.allTasks.push(newTask);
+  };
 }]);
--- test/tasksList.spec.js
+++ test/tasksList.spec.js
@@ -1,14 +1,20 @@
-// describe('TasksController', function(){
-//   beforeEach(module('ToDoList'));
+describe('TasksController', function(){
+  beforeEach(module('ToDoList'));
 
-//   var control;
+  var control;
+  var task = {'task': 'Hello', 'completed': false}
 
-//   beforeEach(inject(function($controller){
-//     control = $controller('TasksController')
-//   }));
+  beforeEach(inject(function($controller){
+    control = $controller('TasksController')
+  }));
 
-//   it('initialises with an empty list', function(){
-//     expect(control.list).toBeUndefined();
-//     expect(control.inputTask).toBeUndefined();
-//   });
-// });
+  it('initialises with an empty list', function(){
+    expect(control.list).toBeUndefined();
+    expect(control.submittedTask).toBeUndefined();
+  });
+
+  it('can store tasks', function(){
+    control.allTasks.push(task);
+    expect(control.allTasks[0].task).toEqual('Hello');
+  });
+});
