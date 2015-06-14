e04423b21fb02ade1b65c7efad69d68d7d413f12
Anna <anna.klimas@gmail.com>
2015-06-14 15:26:47 +0100

    TaskController tested by Karma.

--- js/navigationController.js
+++ js/navigationController.js
@@ -1,3 +1,12 @@
-tasksList.controller("NavigationController", [function(){
+tasksList.controller('NavigationController', [function() {
+  var self = this;
+  self.tab = 0;
 
+  self.setTab = function(selectedTab) {
+    self.tab = selectedTab;
+  };
+
+  self.isSet = function(number) {
+    return self.tab === number;
+  };
 }]);
--- test/tasksList.spec.js
+++ test/tasksList.spec.js
@@ -1,31 +1,53 @@
 describe('TasksController', function(){
-  beforeEach(module('ToDoList'));
 
   var control;
+  var panel;
+
+  beforeEach(module('ToDoList'));
 
   beforeEach(inject(function($controller){
     control = $controller('TasksController')
   }));
+
+
 
   it('initialises with an empty list', function(){
     expect(control.allTasks).toEqual([]);
-    expect(control.submittedTask).toBeUndefined();
   });
-});
 
-describe('TasksController', function(){
-  beforeEach(module('ToDoList'));
-
-  var control;
-  var submittedTask = {'task': 'Hello', 'completed': false}
-
-  beforeEach(inject(function($controller){
-    control = $controller('TasksController')
-  }));
-
-  it('can store and display tasks', function(){
-    control.allTasks.push(submittedTask);
-    expect(control.allTasks[0].task).toEqual('Hello');
+  it('tasks have "not completed" status when created', function(){
+    control.submittedTask = "Buy apples";
+    control.addTasks();
+    expect(control.allTasks[0].completed).toEqual(false)
   });
-});
 
+  it('task status can be changed to "completed"', function(){
+    control.submittedTask = "Buy apples";
+    control.addTasks();
+    control.allTasks[0].completed = true;
+    expect(control.allTasks[0].completed).toEqual(true)
+  });
+
+   it('can store tasks', function(){
+    control.submittedTask = "Buy apples";
+    control.addTasks();
+    expect(control.allTasks[0].task).toEqual('Buy apples');
+  });
+
+   it('can count all tasks', function(){
+    control.submittedTask = "Buy apples";
+    control.addTasks();
+    control.submittedTask = "Buy bananas";
+    control.addTasks();
+    expect(control.allTasks.length).toEqual(2);
+   });
+
+   it('can delete completed tasks', function(){
+    control.submittedTask = "Buy apples"
+    control.addTasks();
+    expect(control.allTasks.length).toEqual(1);
+    control.allTasks[0].completed = true;
+    control.deleteTask(0);
+    expect(control.allTasks.length).toEqual(0);
+   });
+});
