77d53ee072cbc8c23c586b8a7ad1653104d1652c
Anna <anna.klimas@gmail.com>
2015-06-14 19:17:52 +0100

    conf.js amended.

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,19 +1,4 @@
 exports.config = {
-
-  baseUrl: 'http://localhost:8080/app/',
-
-  allScriptsTimeout: 11000,
-
-  specs: ['todoFeature.js'],
-
-  capabilities: {
-    'browserName': 'firefox'
-  },
-
-  framework: 'jasmine',
-
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
-  }
-
-};
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todoFeature.js']
+}
